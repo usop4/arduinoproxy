b94ca7277d8a898a60fe5e8797c0612532f5892c
Takayuki Uehara <t.uehara@gmail.com>
2010-10-25 02:17:39 +0900

    prepare to select formkey and add error handling

--- main.py
+++ main.py
@@ -19,13 +19,14 @@ class MainHandler(webapp.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html; charset=UTF-8'
         self.response.out.write('<a href="/isbn/4873113989">isbn</a><br/ >')
+        self.response.out.write('<a href="/isbn/4873113989/dG5pUF9za3NBYWVTblNMc3FxOGxsWHc6MA">isbn/formkey</a>')
 
 class IsbnHandler(webapp.RequestHandler):
-    def get(self,isbn):
+    def get(self,isbn,formkey='dG5pUF9za3NBYWVTblNMc3FxOGxsWHc6MA'):
         self.response.headers['Content-Type'] = 'text/html; charset=UTF-8'
         booktitle = self.access_rakuten_api(isbn)
-        self.access_google_docs(booktitle)
-        # self.access_google_calendar(booktitle)
+        self.access_google_docs(booktitle,formkey)
+        self.access_google_calendar(booktitle)
         self.response.out.write(booktitle)
 
     def access_rakuten_api(self,isbn):
@@ -33,33 +34,45 @@ class IsbnHandler(webapp.RequestHandler):
         config.read('arduinoproxy.config')
         developerId = config.get('rakuten','developerId')
         url = 'http://api.rakuten.co.jp/rws/3.0/rest?developerId='+developerId+'&operation=BooksBookSearch&version=2010-03-18&isbn=' + isbn
-        xml = urlfetch.fetch(url).content
+        try:
+            xml = urlfetch.fetch(url).content
+        except:
+            return 'connection failed'
         dom = minidom.parseString(xml)
         titles = dom.getElementsByTagName("title")
         subtitles = dom.getElementsByTagName("subTitle")
-        rakuten_result = titles[0].childNodes[0].data
-        return rakuten_result.encode('utf_8')
+        try:
+            rakuten_result = titles[0].childNodes[0].data
+            return rakuten_result.encode('utf_8')
+        except IndexError:
+            return 'Book Not Fount:'+isbn
 
-    def access_google_docs(self,booktitle,formkey='dG5pUF9za3NBYWVTblNMc3FxOGxsWHc6MA'):
+    def access_google_docs(self,booktitle,formkey):
 
         # via http://code.google.com/intl/ja/appengine/docs/python/urlfetch/overview.html
         import urllib
-        
+
+        # self.response.out.write(formkey)
+
+        url = 'http://spreadsheets.google.com/formResponse?formkey='+formkey+'&ifq'
         form_fields = {
             "entry.1.single": booktitle,
             "submit": "submit"
         }
-        urlfetch.fetch(url = 'http://spreadsheets.google.com/formResponse?formkey='+formkey+'&ifq',
-            payload = urllib.urlencode(form_fields),
-            method = urlfetch.POST)
-        
+        try:
+            urlfetch.fetch(url = url,
+                payload = urllib.urlencode(form_fields),
+                method = urlfetch.POST)
+        except:
+            raise
+
     def access_google_calendar(self,booktitle):
 
         config = ConfigParser.ConfigParser()
         config.read('arduinoproxy.config')
 
         calendar_service = gdata.calendar.service.CalendarService()
-        calendar_service.email = 't.uehara@gmail.com'
+        calendar_service.email = config.get('google','id')
         calendar_service.password = config.get('google','pw')
         calendar_service.source = 'Example-Example-1'
         calendar_service.ProgrammaticLogin()
@@ -77,8 +90,8 @@ class IsbnHandler(webapp.RequestHandler):
 def main():
     application = webapp.WSGIApplication([
         ('/', MainHandler),
+        ('/isbn/([0-9]{10})/(.*)', IsbnHandler),
         ('/isbn/(.*)', IsbnHandler),
-        ('/isbn/(.*)/(.*)', IsbnHandler),
     ],debug=True)
     util.run_wsgi_app(application)
 
